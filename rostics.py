dda2ef7faa555f84134b7c30fd753551bd76a421
saikou1337 <99181676+saikou1337@users.noreply.github.com>
2026-03-12 16:51:56 +0500

    Update rostics.py

--- rostics.py
+++ rostics.py
@@ -18,3 +18,25 @@ def hello(message):
   bot.send_message(message.chat.id, akciya)
 
 bot.polling()
+
+
+
+
+import requests
+import telebot
+from bs4 import BeautifulSoup
+import os
+
+API_KEY = os.environ.get('TELEGRAM_BOT_TOKEN', '8602811245:AAEYXcNpGMH9WUmT57qC_YAKUAJewdDJ3Ps')
+bot = telebot.TeleBot(API_KEY)
+def parser():
+    URL = 'https://rostics.ru/promo/crazydays'
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    element = soup.find('h1', class_='Nq_zsMVbqV3')
+  
+@bot.message_handler(commands=['начать'])
+def hello(message):
+    akciya = parser()
+    bot.send_message(message.chat.id, akciya)
+bot.polling()
