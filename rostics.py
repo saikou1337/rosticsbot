05fa70d22369f3eb935110b64bf4a18e88b41915
saikou1337 <99181676+saikou1337@users.noreply.github.com>
2026-03-13 08:08:18 +0500

    Update rostics.py

--- rostics.py
+++ rostics.py
@@ -1,39 +1,15 @@
 import requests
 import telebot
 from bs4 import BeautifulSoup
-
-API_KEY = '8602811245:AAEYXcNpGMH9WUmT57qC_YAKUAJewdDJ3Ps'
-bot = telebot.TeleBot(API_KEY)
-def parser():
-  URL = 'https://rostics.ru/promo/crazydays'
-  
-  r = requests.get(URL)
-  soup = BeautifulSoup(r.text, 'html.parser')
-  
-  akciya = soup.find('h1', class_='Nq_zsMVbqV3').text.strip()
-
-
-@bot.message_handler(commands=['начать'])
-def hello(message):
-  bot.send_message(message.chat.id, akciya)
-
-bot.polling()
-
-
-
-
-import requests
-import telebot
-from bs4 import BeautifulSoup
 import os
 
 API_KEY = os.environ.get('TELEGRAM_BOT_TOKEN', '8602811245:AAEYXcNpGMH9WUmT57qC_YAKUAJewdDJ3Ps')
 bot = telebot.TeleBot(API_KEY)
 def parser():
-    URL = 'https://rostics.ru/promo/crazydays'
+    URL = 'https://rostics.ru'
     r = requests.get(URL)
     soup = BeautifulSoup(r.text, 'html.parser')
-    element = soup.find('h1', class_='Nq_zsMVbqV3')
+    element = soup.find('div', class_='_14ZQf5wtqxX')
   
 @bot.message_handler(commands=['начать'])
 def hello(message):
