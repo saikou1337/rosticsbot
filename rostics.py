4b6d1c2b5d8e192db18ed2002d5420e20e81cc29
saikou1337 <99181676+saikou1337@users.noreply.github.com>
2026-03-18 08:07:28 +0500

    Update rostics.py

--- rostics.py
+++ rostics.py
@@ -2,13 +2,13 @@ import requests
 import telebot
 from bs4 import BeautifulSoup
 
-URL = 'https://rostics.ru/promo'
+URL = 'https://rostics.ru/promo/crazydays'
 API_KEY = '8602811245:AAEYXcNpGMH9WUmT57qC_YAKUAJewdDJ3Ps'
 bot = telebot.TeleBot(API_KEY)
 def parser():
     r = requests.get(URL)
     soup = BeautifulSoup(r.text, 'html.parser')
-    element = soup.find('h1', class_='_1-7Ufy1g4eD h1')
+    element = soup.find('h1', class_='Nq_zsMVbqV3')
     return [c.text for c in element]
   
 @bot.message_handler(commands=['5050'])
